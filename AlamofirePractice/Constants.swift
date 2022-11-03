9c07d92eff951cbfceed39f9126ab230fc292717
shin seung jae <shinseungjae1234@gmail.com>
2022-11-03 12:29:14 +0900

    feat: search버튼 누를 시 api 호출

---
+++ AlamofirePractice/Constants.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+enum API {
+    static let Base_URL: String = "https://api.unsplash.com/"
+    static let CLIENT_ID: String = "JMo_6uU23jsUZpMIFYl43epxuBSBRo7AAPk-5FRcnLI"
+}
--- AlamofirePractice/ViewController.swift
+++ AlamofirePractice/ViewController.swift
@@ -1,12 +1,26 @@
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var searchTextField: UITextField!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func didTabSearchButton(_ sender: Any) {
+        guard let search = searchTextField.text else { return }
+        let url = "\(API.Base_URL)search/photos"
+        
+        // 파라미터 사용
+        let queryParam = ["query" : search, "client_id" : API.CLIENT_ID]
+        
+        AF.request(url, method: .get, parameters: queryParam).response(completionHandler: { response in debugPrint(response)})
+    }
+    
 }
 
