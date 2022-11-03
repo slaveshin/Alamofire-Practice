fcad38a59d8090195f469746f5c38e9f3ea00a78
shin seung jae <shinseungjae1234@gmail.com>
2022-11-03 11:13:37 +0900

    edit initproject

---
+++ AlamofirePractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlamofirePractice
+//
+//  Created by 신승재 on 2022/10/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
