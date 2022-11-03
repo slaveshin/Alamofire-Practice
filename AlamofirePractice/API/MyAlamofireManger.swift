79349c8f94dc43d206e15c41ecf6acabc7de2d3e
shin seung jae <shinseungjae1234@gmail.com>
2022-11-03 16:13:49 +0900

    add: BaseInterceptor.swift 추가

---
+++ AlamofirePractice/API/MyAlamofireManger.swift
@@ -0,0 +1,19 @@
+import Foundation
+import Alamofire
+
+// singletone
+final class MyAlamofireManger {
+    
+    static let shared = MyAlamofireManger()
+    
+    // intercepter
+    // let intercepters = Interceptor()
+    
+    
+    // 로거 설정
+    // let monitors =
+    
+    // 세션 설정
+    var session = Session.default
+    
+}
--- AlamofirePractice/MyAlamofireManger.swift
+++
@@ -1,8 +0,0 @@
-//
-//  MyAlamofireManger.swift
-//  AlamofirePractice
-//
-//  Created by 신승재 on 2022/11/03.
-//
-
-import Foundation
